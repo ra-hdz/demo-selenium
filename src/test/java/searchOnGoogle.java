c7fcaa4e2d0d35b863f7f0a48e22a22b1f5c45c0
ra-hdz <hernandezsanchezra@outlook.com>
2022-08-28 20:43:18 -0500

    Launch Chrome on headless mode

--- src/test/java/searchOnGoogle.java
+++ src/test/java/searchOnGoogle.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,6 +16,10 @@ public class searchOnGoogle {
     @Test
     public void launchBrowser(){
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920x1080");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();

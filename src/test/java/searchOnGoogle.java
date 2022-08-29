67cdcee67fce65096d030446877a26d9de030a46
ra-hdz <hernandezsanchezra@outlook.com>
2022-08-28 19:55:06 -0500

    Demo test: Search 'longcat' on Google

---
+++ src/test/java/searchOnGoogle.java
@@ -0,0 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class searchOnGoogle {
+
+    WebDriver driver;
+
+    @Test
+    public void launchBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+    }
+
+    @Test(dependsOnMethods = "launchBrowser")
+    public void navigateToGoogle() {
+        driver.get("https://www.google.com/");
+    }
+
+    @Test(dependsOnMethods = "navigateToGoogle")
+    public void typeIntoSearchbox() {
+        WebElement searchbox = driver.findElement(By.xpath("//form[@action='/search']//input[@type='text']"));
+
+        searchbox.sendKeys("longcat");
+    }
+
+    @Test(dependsOnMethods = "typeIntoSearchbox")
+    public void clickSearch() {
+        WebElement searchButton = driver.findElement(By.xpath("(//form[@action='/search']//input[@name='btnK'][1])[1]"));
+        searchButton.click();
+    }
+
+    @Test(dependsOnMethods = "clickSearch")
+    public void resultsPageIsReached() {
+        Assert.assertTrue(driver.getCurrentUrl().contains("search?q=longcat"));
+    }
+
+    @Test(dependsOnMethods = "resultsPageIsReached")
+    public void closeBrowser() {
+        driver.close();
+    }
+}
